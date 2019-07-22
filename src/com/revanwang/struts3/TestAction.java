a5e67d92eb9c02dd7df7da84892c657aad13d618
RevanWang <zjqx1991@163.com>
2019-07-22 17:57:52 +0800

    使用通配符来配置资源

---
+++ src/com/revanwang/struts3/TestAction.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.revanwang.struts3;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @Desc 	
+ * @author Revan Wang
+ *
+ * @Date Jul 22, 20193:45:02 PM
+ */
+public class TestAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+	
+	public void test1() {
+		System.out.println("TestAction。。。。。test1");
+	}
+
+	public void test2() {
+		System.out.println("TestAction。。。。。test2");
+	}
+
+	public void test3() {
+		System.out.println("TestAction。。。。。test3");
+	}
+
+	public void test4() {
+		System.out.println("TestAction。。。。。test4");
+	}
+
+	public void test5() {
+		System.out.println("TestAction。。。。。test5");
+	}
+}
