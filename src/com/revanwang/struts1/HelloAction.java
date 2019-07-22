ec57ed1d9c7cce715c12c9da309cb60ebc4b8ee9
RevanWang <zjqx1991@163.com>
2019-07-22 14:10:36 +0800

    struts2初体验

---
+++ src/com/revanwang/struts1/HelloAction.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.revanwang.struts1;
+
+/**
+ * @Desc 	
+ * @author Revan Wang
+ *
+ * @Date Jul 22, 201912:02:35 PM
+ */
+public class HelloAction {
+
+	public String sayHello() {
+		System.out.println("Hello Struts2....");
+		return "hi";
+	}
+}
